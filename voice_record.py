3cdb474df698de7914c362fac750d7f3e7dd9c2d
Stephanie Salas <A00835015@tec.mx>
2026-06-01 13:52:29 -0600

    new Audios

--- voice_record.py
+++ voice_record.py
@@ -18,7 +18,7 @@ duracion = 1
 # Palabra que se desea grabar.
 # Se puede cambiar por otra palabra del conjunto,
 # por ejemplo: "inicio", "alto", "pausa", "ruta", etc.
-palabra = "freno"
+palabra = "entrega"
 
 # ---------------------------------------------------------
 # CREACIÓN DE LA CARPETA PARA GUARDAR LAS GRABACIONES
